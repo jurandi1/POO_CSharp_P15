f5e87d3a9d5f5b4b1252260b443d78db2db4372f
Jurandi Junior <jurandijr20@gmail.com>
2025-02-09 21:14:12 -0300

    Solução sem interface - PARTE 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using POO_CSharp_P15.Entities;
+using POO_CSharp_P15.Services;
 
 namespace POO_CSharp_P15
 {
@@ -15,9 +16,19 @@ namespace POO_CSharp_P15
             Console.Write("Return (dd/MM/yyyy hh:mm): ");
             DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
+            RentalService rentalService = new   RentalService(hour, day);
 
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
--- Services/RentalService.cs
+++ Services/RentalService.cs
@@ -7,6 +7,10 @@ namespace POO_CSharp_P15.Services
         public double PricePerHour { get; private set; }
         public double PricePerDay { get; private set; }
 
+        private BrazilTaxService _brazilTaxService =  new BrazilTaxService();
+
+
+
         public RentalService(double pricePerHour, double pricePerDay)
         {
             PricePerHour = pricePerHour;
@@ -15,6 +19,22 @@ namespace POO_CSharp_P15.Services
 
         public void ProcessInvoice(CarRental carRental)
         {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+
 
         }
     }
